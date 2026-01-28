60847866b834fe70c856c269cda8506ce7b309ee
nightmoon <ovs246110@kcsf2014.onmicrosoft.com>
2026-01-28 13:53:04 +0900

    aa

--- 総合演習/src/main/java/Servlet/MypageServlet.java
+++ 総合演習/src/main/java/Servlet/MypageServlet.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import dao.UserDao;
 import model.User;
 
 @WebServlet("/Mypage")
@@ -22,34 +21,25 @@ public class MypageServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException { 
-    	
-    	//JSPからユーザーのIDと名前を取得
-    	int id = Integer.parseInt(request.getParameter("userid"));
-    	String name = request.getParameter("username");
-    	
     	//セッションの取得
-        HttpSession session = request.getSession();
-        
-        //DAOからユーザー情報を取得
-        User user = (User) session.getAttribute("loginuser");
-        UserDao dao = new UserDao();
-        
-        dao.findIdAndPass(id, name);
-        
-        // 未ログインならログイン画面へ
-        if (user == null) {
-            response.sendRedirect("login.jsp");
-            return;
-        }
-        
-        // JSPに渡す
-        request.setAttribute("user", user);
-        
-        //現在の画面の情報をJSPに渡す
-        request.setAttribute("currentPage","Mypage");
-        
-        //フォワード
-        request.getRequestDispatcher("mypage.jsp").forward(request, response);
+    	HttpSession session = request.getSession(false);
+    	
+    	// 未ログインならログイン画面へ
+    	if (session == null || session.getAttribute("loginuser") == null) {
+    	 response.sendRedirect("login.jsp");
+    	 return;
+     }
+    	
+    	//ログインユーザーを取得
+    	User user = (User)session.getAttribute("loginuser");
+    	
+    	 // JSPに渡す
+    	request.setAttribute("user", user);
+    	//現在の画面の情報をJSPに渡す
+    	request.setAttribute("currentPage","Mypage"); 
+    	
+    	//フォワード
+    	request.getRequestDispatcher("mypage.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
