0871e714e47496bc32144a58d2c5e4f82eaa28da
246145 <246145@KCSF0948.KCS-Learning.local>
2026-01-28 12:20:22 +0900

    CreateNewUserServlet完成（動作テスト待ち）

--- 総合演習/src/main/java/Servlet/CreateNewUserServlet.java
+++ 総合演習/src/main/java/Servlet/CreateNewUserServlet.java
@@ -1,6 +1,7 @@
 package Servlet;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Random;
 
 import jakarta.servlet.RequestDispatcher;
@@ -9,8 +10,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
+import dao.UserDao;
 import model.User;
 
 @WebServlet("/CreateNewUserServlet")
@@ -24,9 +25,6 @@ public class CreateNewUserServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		HttpSession session = request.getSession();
-		User user = (User) session.getAttribute("user");
-
 		RequestDispatcher dispatcher;
 		dispatcher = request.getRequestDispatcher("/createNewUser.html");
 		dispatcher.forward(request, response);
@@ -35,19 +33,26 @@ public class CreateNewUserServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		CreateNewUserServlet CNUserv = new CreateNewUserServlet();
+		UserDao userDAO = new UserDao();
+		RequestDispatcher dispatcher;
 
 		String name = request.getParameter("userName");
 		String password = request.getParameter("password");
 		String confirmPassword = request.getParameter("confirmPassword");
+		int iconNum = 0;
 
 		//新規ユーザーのユーザーIDを作成
 		int userID = CNUserv.CreateID();
 
 		//パスワードと確認用パスワードが同じなら登録へ
 		if (password.equals(confirmPassword)) {
-			HttpSession session = request.getSession();
 
-			User newUser = new User(userID, password, name, 0);
+			User newUser = new User(userID, password, name, iconNum);
+			userDAO.createUser(newUser);
+
+			//登録が完了したら部屋選択画面へ遷移する。
+			dispatcher = request.getRequestDispatcher("/RoomSelection");
+			dispatcher.forward(request, response);
 
 		} else {
 			//違ったらユーザー登録画面へ
@@ -57,22 +62,20 @@ public class CreateNewUserServlet extends HttpServlet {
 
 	public int CreateID() {
 		Random rand = new Random();
+		UserDao userDAO = new UserDao();
+
 		//ランダムな6桁の数値を入力
-		int kari = rand.nextInt(100000, 999999);
+		int newID;
 
 		//作ったIDが既存のIDと被りがないかのチェック
-		//ここから下に書く
 		while (true) {
-			//数値と変数、if文条件の仮置き
-			int i = 0;
-			if (i == 0) {
-
-			} else {
+			newID = rand.nextInt(100000, 999999);
+			User user = userDAO.findById(newID);
+			if (Objects.isNull(user)) {
 				break;
 			}
 		}
-
-		return kari;
+		return newID;
 	}
 
 }
