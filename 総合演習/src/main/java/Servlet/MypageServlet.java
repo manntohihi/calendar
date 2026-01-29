ed0f7f8ec083ebbff99c4de65f06845e8515aa7c
246145 <246145@KCSF0948.KCS-Learning.local>
2026-01-29 11:42:39 +0900

    a

--- 総合演習/src/main/java/Servlet/MypageServlet.java
+++ 総合演習/src/main/java/Servlet/MypageServlet.java
@@ -13,37 +13,37 @@ import model.User;
 
 @WebServlet("/Mypage")
 public class MypageServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public MypageServlet() {
-        super();
-    }
+	public MypageServlet() {
+		super();
+	}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException { 
-    	//セッションの取得
-    	HttpSession session = request.getSession(false);
-    	
-    	// 未ログインならログイン画面へ
-    	if (session == null || session.getAttribute("loginUser") == null) {
-    	 response.sendRedirect("login.jsp");
-    	 return;
-     }
-    	
-    	//ログインユーザーを取得
-    	User user = (User)session.getAttribute("loginUser");
-    	
-    	 // JSPに渡す
-    	request.setAttribute("user", user);
-    	//現在の画面の情報をJSPに渡す
-    	request.setAttribute("currentPage","Mypage"); 
-    	
-    	//フォワード
-    	request.getRequestDispatcher("mypage.jsp").forward(request, response);
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//セッションの取得
+		HttpSession session = request.getSession(false);
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doGet(request, response);
-    }
+		// 未ログインならログイン画面へ
+		if (session == null || session.getAttribute("loginUser") == null) {
+			response.sendRedirect("Login.jsp");
+			return;
+		}
+
+		//ログインユーザーを取得
+		User user = (User) session.getAttribute("loginUser");
+
+		// JSPに渡す
+		request.setAttribute("user", user);
+		//現在の画面の情報をJSPに渡す
+		request.setAttribute("currentPage", "Mypage");
+
+		//フォワード
+		request.getRequestDispatcher("mypage.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
