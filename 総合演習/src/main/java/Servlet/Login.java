d62b827535763db56b355fde8db5fadd477c1e71
246145 <246145@KCSF0948.KCS-Learning.local>
2026-02-02 12:01:53 +0900

    Merge branch 'master' of https://github.com/manntohihi/calendar.git

--- 総合演習/src/main/java/Servlet/Login.java
+++ 総合演習/src/main/java/Servlet/Login.java
@@ -1,8 +1,7 @@
 package Servlet;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -21,40 +20,45 @@ import model.User;
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher dispatcher;
 		dispatcher = request.getRequestDispatcher("/Login.jsp");
-		dispatcher.forward(request,response);
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("Login.java");
-		int ID = Integer.parseInt( request.getParameter("ID"));
+		int ID = Integer.parseInt(request.getParameter("ID"));
 		String password = request.getParameter("password");
 		User user = new User();
 		user.setUserId(ID);
 		user.setPasswd(password);
 		UserDao ud = new UserDao();
-		List<User> userList = new ArrayList<User>();
-		userList= ud.login(user);
+		//		List<User> userList = new ArrayList<User>();
+		User loginUser = new User();
+		loginUser = ud.login(user);
 		RequestDispatcher dispatcher;
 		String name = null;
-		System.out.println(userList.size()+"size");
+		System.out.println(loginUser + "size");
+
+		/*
 		for(User use : userList) {
 			System.out.println("f2");
 			name = use.getUserName();
@@ -69,9 +73,24 @@ public class Login extends HttpServlet {
 			}
 			System.out.println("end");
 		}
+		*/
+
+		name = loginUser.getUserName();
+		System.out.println("f2");
+		if (Objects.isNull(name)) {
+			dispatcher = request.getRequestDispatcher("/LoginError.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", loginUser);
+			dispatcher = request.getRequestDispatcher("/RoomSelection");
+			dispatcher.forward(request, response);
+		}
+		System.out.println("end");
+
 		dispatcher = request.getRequestDispatcher("/LoginError.jsp");
 		System.out.println("eroor");
-		dispatcher.forward(request,response);
+		dispatcher.forward(request, response);
 		//テスト
 		/*
 		if(ID==111111) {
@@ -84,7 +103,7 @@ public class Login extends HttpServlet {
 			dispatcher.forward(request,response);
 		}
 		*/
-		
+
 	}
 
 }
