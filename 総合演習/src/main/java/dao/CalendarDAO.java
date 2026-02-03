2412d65d4b8e83e880d40577aa2b5875c4bac731
manntohihi <ovs246133@kcsf2014.onmicrosoft.com>
2026-02-03 11:19:58 +0900

    a

--- 総合演習/src/main/java/dao/CalendarDAO.java
+++ 総合演習/src/main/java/dao/CalendarDAO.java
@@ -157,7 +157,7 @@ public  CalendarEvent findEntryrDate(int loginUserId,int GroupId,String title){
 
 
 
-	public void setCalendarDate(int group_id,String title,String texdescription, LocalDateTime start_datetime,LocalDateTime end_datetime,int created_by){
+	public void setCalendarDate(int group_id,String title,String texdescription, LocalDateTime start_datetime,LocalDateTime end_datetime,int created_by,String color){
 		CalendraEventList PostPrductLogic = null;
 		List<CalendarEvent> CalendarEventList = new ArrayList();
 		
@@ -181,8 +181,8 @@ public  CalendarEvent findEntryrDate(int loginUserId,int GroupId,String title){
 		//SELCT文を準備
 				String sql =
 				"INSERT INTO calendar_events " +
-				"(groupID, title, description, start_datetime, end_datetime, created_by) " +
-				"VALUES (?, ?, ?, ?, ?, ?)";
+				"(groupID, title, description, start_datetime, end_datetime, created_by,color) " +
+				"VALUES (?, ?, ?, ?, ?, ?, ?)";
 
 				PreparedStatement ps = conn.prepareStatement(sql);
 				
@@ -192,6 +192,7 @@ public  CalendarEvent findEntryrDate(int loginUserId,int GroupId,String title){
 				ps.setTimestamp(4, Timestamp.valueOf(start_datetime));
 				ps.setTimestamp(5, Timestamp.valueOf(end_datetime));
 				ps.setLong(6, created_by);
+				ps.setString(7, color);
 				System.out.println(ps);
 				ps.executeUpdate();
 		
